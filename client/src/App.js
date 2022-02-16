3e902a2f6df2fe5ee39c7d1da98361421ff4a0fa
Christian Dalfonso <christiandalfonso@gmail.com>
2022-02-16 17:22:09 -0500

    Add Desktop Component to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
-import "./App.css";
+import "./App.scss";
+
+import Desktop from "./Components/Desktop/Desktop";
+
+// import "/assets/.scss";
 
 class App extends Component {
   state = {
@@ -26,11 +30,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">{this.state.data}</p>
+        <Desktop></Desktop>
       </div>
     );
   }
